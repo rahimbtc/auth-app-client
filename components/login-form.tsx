00365f2257b51779e7aec751581f55499ac44815
Brahim <bra-95@live.fr>
2022-10-21 00:13:04 +0100

    finished login page style

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,25 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 import Image from 'next/image'
-import styles from '../styles/login-form.module.css';
+import styles from '../styles/Login.module.scss';
 
 interface Values {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export default function LoginForm() {
-    return (
-      <div className={styles.rectangle184}>
+  return (
+    <div className={styles["login-container"]}>
+      <div className={styles["login"]}>
         <a
           href="#"
           rel="noopener noreferrer"
+          className={styles.devchallenges}
         >
-          <span className={styles.path44}>
-            <Image src="/devchallenges.png" alt="devchallenges Logo" width="36.87px" height="18px" />
-          </span>
+          <img src="/devchallenges.svg" alt="devchallenges Logo" />
         </a>
-        <h3 className={styles.text1 + ' p-3'}>Join thousands of learners from around the world </h3>
-        <h5 className={styles.text2 + ' p-3'}>Master web development by making real-life projects. There are multiple paths for you to choose </h5>
+        <h3>Join thousands of learners from around the world </h3>
+        <p>Master web development by making real-life projects. There are multiple paths for you to choose </p>
         <Formik
           initialValues={{
             username: '',
@@ -38,27 +39,47 @@ export default function LoginForm() {
 
         >
           <Form>
-            <div className={styles.rectangle181 + ' mb-3'}>
-              <Field className={styles.email + "form-control"} id="email" name="email" placeholder="Email" aria-describedby="emailHelp" />
+            <div className={styles["fields-container"]}>
+              <Field className={styles.email} id="email" name="email" placeholder="Email" aria-describedby="emailHelp" />
+              <Field className={styles.password} id="password" name="password" placeholder="Password" type="password" />
             </div>
-  
-            <div className={styles.rectangle182 + ' mb-3'}>
-              <Field className="form-control" id="password" name="password" placeholder="Password" type="password" />
-            </div>
-
-            <button type="submit" className={styles.rectangle183}>Login</button>
+            <button type="submit" className={styles["login-button"]}>Login</button>
           </Form>
         </Formik>
-        <p className={styles.text3 + ' p-3'}>or continue with these social profile </p>
-
-        <a
-          href="#"
-          rel="noopener noreferrer"
-        >
-          <span className={styles.ellipse2}>
-            <Image src="/Gihub.svg" alt="GITHUB Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p className={styles["small-gray-text"]}>or continue with these social profiles </p>
+        <div className={styles["social-links"]}>
+          <a
+            href="#"
+            rel="noopener noreferrer"
+          >
+            <img src="/Google.svg" alt="GITHUB Logo" />
+          </a>
+          <a
+            href="#"
+            rel="noopener noreferrer"
+          >
+            <img src="/Facebook.svg" alt="GITHUB Logo" />
+          </a>
+          <a
+            href="#"
+            rel="noopener noreferrer"
+          >
+            <img src="/Twitter.svg" alt="GITHUB Logo" />
+          </a>
+          <a
+            href="#"
+            rel="noopener noreferrer"
+          >
+            <img src="/Gihub.svg" alt="GITHUB Logo" />
+          </a>
+        </div>
+        <p className={styles["small-gray-text"]}>Already a member?
+          <a href="#"
+            rel="noopener noreferrer">
+            <span className={styles["small-blue-text"]}>Login</span>
+          </a>
+        </p>
       </div>
-    );
-  };
+    </div>
+  );
+};
